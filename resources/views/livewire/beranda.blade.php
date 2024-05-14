26e530a649e432573bf098dabddb83019765a240
vinwithin <kelvinadinata82@gmail.com>
2024-05-14 13:21:37 +0700

    feat: beranda

--- resources/views/livewire/beranda.blade.php
+++ resources/views/livewire/beranda.blade.php
@@ -1,43 +1,207 @@
 <div>
-    <div class="bg-dark vh-100 justify-content-center align-items-center ms-auto">
-        <nav class="navbar navbar-expand-lg text-center">
+    <div class="bg-dark vh-100 d-flex flex-column justify-content-center align-items-center ms-auto">
+        <nav class="navbar navbar-expand-lg text-center border-bottom border-danger">
             <div class="container-fluid mx-auto">
-                <a class="navbar-brand  text-white" href="#">Navbar</a>
+                <a class="navbar-brand text-white" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mx-auto ">
+                    <ul class="navbar-nav mx-auto ml-2">
                         <li class="nav-item">
-                            <a class="nav-link active  text-white" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active text-white" aria-current="page" href="#">Beranda</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link  text-white" href="#">Features</a>
+                            <a class="nav-link text-white" href="#">Visi Misi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Pricing</a>
+                            <a class="nav-link text-white" href="#">Struktur Organisasi</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-white" aria-disabled="true">Disabled</a>
+                            <a class="nav-link text-white" href="#">Berita</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Layanan</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <div class="container d-flex w-75 h-100 justify-content-start align-items-center text-white ml-3 ">
-            <h1>Kelurahan Kebun Handil <br> Kecamatan Jelutung <br> Kota Jambi</h1>
+        <div class="container d-flex w-75 h-75 justify-content-start align-items-center text-white ml-3">
+            <h1 class="fw-bold">Kelurahan Kebun Handil <br> Kecamatan Jelutung <br> Kota Jambi</h1>
+        </div>
+        <div class="w-100" style="background-image: url('image/Asset1.png'); background-size: cover; background-position: center;">
+            <h1 class=" p-5">k</h1>
         </div>
     </div>
-    <div class="bg-light vh-100">
-        <div class="container w-75 ">
+    <div class="vh-100">
+        <div class="container w-75">
             <div class="mt-5">
-                <h1>Blog</h1>
+                <h1 class="fw-bold">Blog</h1>
             </div>
             <div>
-                <p>Informasi atau Berita Terkini</p>
+                <p class="border-bottom border-danger">Informasi atau Berita Terkini</p>
             </div>
-            
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5">
+                <h1 class="border-bottom border-danger fw-bold">Fasilitas Umum</h1>
+              </div>
+              <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5">
+                <h1 class="border-bottom border-danger">Kegiatan dan Acara</h1>
+              </div>
+              <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        
         </div>
+        <footer class="bg-dark text-white mt-5">
+            <div class="container">
+                <div class="row py-4">
+                    <div class="col-md-3">
+                        <h5>Kelurahan Kebun Handil <br> Kecamatan Jelutung <br> Kota Jambi</h5>
+                        
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Home</a></li>
+                            <li><a href="#" class="text-white">Blog</a></li>
+                            <li><a href="#" class="text-white">Fasilitas Umum</a></li>
+                            <li><a href="#" class="text-white">Kegiatan dan Acara</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Visi Misi</a></li>
+                            <li><a href="#" class="text-white">Struktur Organisasi</a></li>
+                            <li><a href="#" class="text-white">Layanan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>Contact Us</h5>
+                        <ul class="list-unstyled">
+                            <li><span class="font-weight-bold">Address:</span> 123 Street Name, City, State, ZIP</li>
+                            <li><span class="font-weight-bold">Phone:</span> (123) 456-7890</li>
+                            <li><span class="font-weight-bold">Email:</span> info@example.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="text-center py-3">
+                    <p>&copy; Copyright Kelurahan Kebun Handil, Kec. Jelutung, Kota Jambi 2024. All Rights Reserved.<p>
+                </div>
+            </div>
+        </footer>
     </div>
+    
+    
 </div>
